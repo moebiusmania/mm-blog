699cb30bc46e815876e314481ee20fad2ca4f24c
Salvatore Laisa <salvatore.laisa@tui.com>
2021-12-27 10:39:12 +0100

    fix: trying again to fix assets paths...

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  exportTrailingSlash: true,
   reactStrictMode: true,
-  assetPrefix: isProd ? "/mm-blog/" : "",
+  assetPrefix: isProd ? "/mm-blog" : "",
   basePath: isProd ? "/mm-blog" : "",
 };
