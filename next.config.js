8b639fd1b0c877b3cc8c197e1893a72680014c1a
Salvatore Laisa <salvatore.laisa@tui.com>
2021-12-27 10:29:02 +0100

    fix: update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  exportTrailingSlash: true,
   reactStrictMode: true,
   assetPrefix: isProd ? "/mm-blog/" : "",
   basePath: isProd ? "/mm-blog" : "",
